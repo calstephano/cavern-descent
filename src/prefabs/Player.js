8d71e09b00389159ed540263a80d1d2dae09dfbc
TeiHek <lerandy1235@gmail.com>
2022-05-08 23:17:40 -0700

    Set up Player for movement

---
+++ src/prefabs/Player.js
@@ -0,0 +1,38 @@
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.moveSpeed = game.settings.moveSpeed; // Assign move speed
+        this.direction = 'down';                  // Store the direction after walking
+        // Create animations
+
+    }
+
+    update() {
+        if (keyA.isDown) {
+            // Play left walk anim
+            this.setVelocityX(-this.moveSpeed);
+        } else if (keyD.isDown) {
+            // Play right walk anim
+            this.setVelocityX(this.moveSpeed);
+        } else if (keyW.isDown) {
+            // Play up walk anim
+            this.setVelocityY(-this.moveSpeed);
+        } else if (keyS.isDown) {
+            // Play down walk anim
+            this.setVelocityY(this.moveSpeed);
+        } else {
+            if (this.direction == 'left') {
+                // Play left idle anim
+            } else if (this.direction == 'right') {
+                // Play right idle anim
+            } else if (this.direction == 'up') {
+                // Play up idle anim
+            } else {
+                // Play down idle anim
+            }
+        }
+    }
+}
