cc015bc4b02546ba50356483351f5993faf2a64b
TeiHek <lerandy1235@gmail.com>
2022-05-27 12:54:25 -0700

    Added placeholder tiles for level1

--- src/scenes/Level1.js
+++ src/scenes/Level1.js
@@ -4,16 +4,103 @@ class Level1 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('protomap', './assets/Concept_Map.png');
         // Add enemy images/atlases here
         this.load.image('enemy1', './assets/tempEnemy1.png');
         this.load.image('enemy2', './assets/tempEnemy2.png');
         this.load.image('bullet', './assets/Square.png');
         this.load.image('darkness', './assets/darkness.png')
+
+        // Tilemap
+        this.load.tilemapTiledJSON('level1_map', './assets/level1.json');
+        this.load.spritesheet('level1tiles', './assets/placeholder.png', {
+            frameWidth: 64,
+            frameHeight: 64,
+            spacing: 0
+        });
     }
 
     create() {
-    // Delete everything after this line, these are for testing
+        this.doorSFX = this.sound.add('doorEnter');
+        const map = this.add.tilemap('level1_map');
+        const tileset = map.addTilesetImage('placeholder', 'level1tiles');
+        const groundLayer = map.createLayer('Ground', tileset);
+        const entranceLayer = map.createLayer('Entrance', tileset);
+        const TopWallsLayer = map.createLayer('TopWalls', tileset);
+        let p1Spawn = map.findObject("Objects", obj => obj.name === "playerSpawn");
+
+
+        // Add enemy groups
+        this.EGroups = new EnemyGroups(this, 'enemy1', 'enemy2', 'bullet');
+        this.EGroups.addEnemyGroups();
+
+        // get basic enemy object array from tilemap Objects layer
+        let BasicEnemyObjects = map.filterObjects("Objects", obj => obj.name === "basicEnemy");
+        // get enemy object array from tilemap Objects layer
+        let RangedEnemyObjects = map.filterObjects("Objects", obj => obj.name === "rangedEnemy");
+
+        // set up player
+        this.gameOver = false;
+        this.p1 = new Player(this, p1Spawn.x, p1Spawn.y, "idleAtlas", 'IdleDown_0001', false);
+        this.p1.setupCombat(true);
+
+        // Create lower walls + collisions for both top and bottom walls
+        const BottomWallsLayer = map.createLayer('Walls', tileset);
+        TopWallsLayer.setCollisionByProperty({ 
+            collides: true 
+        });
+        this.physics.add.collider(this.p1, TopWallsLayer);
+        BottomWallsLayer.setCollisionByProperty({ 
+            collides: true 
+        });
+        this.physics.add.collider(this.p1, BottomWallsLayer);
+
+        // Add enemy
+        BasicEnemyObjects.map((element) => {
+            this.EGroups.addBasicEnemy(element.x, element.y, this.p1, 400, 200);
+        })
+        RangedEnemyObjects.map((element) => {
+            this.EGroups.addRangedEnemy(element.x, element.y, this.p1, 500, 200);
+        })
+        this.physics.add.collider(this.EGroups.BEGroup, BottomWallsLayer)
+        this.physics.add.collider(this.EGroups.BEGroup, TopWallsLayer)
+        this.physics.add.collider(this.EGroups.REGroup, BottomWallsLayer)
+        this.physics.add.collider(this.EGroups.REGroup, TopWallsLayer)
+        this.physics.add.collider(this.EGroups.bulletGroup, BottomWallsLayer, (bullet, wall) => {
+            bullet.kill();
+        });
+        this.physics.add.collider(this.EGroups.bulletGroup, TopWallsLayer, (bullet, wall) => {
+            bullet.kill();
+        });
+        //this.cameras.main.setBounds(0, 0, map.width, map.height);
+        this.cameras.main.startFollow(this.p1, true, 0.1, 0.1);
+
+        // Only call scene change exactly once on entrance
+        this.inEntrance = false;
+        this.physics.add.overlap(this.p1, entranceLayer, ()=> {
+            if (!this.inEntrance) {
+                this.inEntrance = true
+                this.doorSFX.play();
+                this.scene.start('door2Scene');
+            }
+        }, this.checkOverlap, this);
+        
+        // Set up keyboard controls for player
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+
+        // Test that the walls actualy have collision
+        // const debugGraphics = this.add.graphics().setAlpha(0.75);
+        // wallsLayer.renderDebug(debugGraphics, {
+        //     tileColor: null,    // color of non-colliding tiles
+        //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),    // color of colliding tiles
+        //     faceColor: new Phaser.Display.Color(40, 39, 37, 255)                // color of colliding face edges
+        // });
+        
+        /*
+        // Delete everything after this line, these are for testing
         // Keyboard controls
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -63,12 +150,8 @@ class Level1 extends Phaser.Scene {
         // this.darkness2.setAlpha(0.5);
         // this.darkness3.setAlpha(0.5);
         // this.darkness4.setAlpha(0.5);
-    }
 
-    update() {
-        if(!this.gameOver) {
-            this.playertest.update();
-            // this.darkness.x = this.playertest.x;
+        // this.darkness.x = this.playertest.x;
             // this.darkness.y = this.playertest.y;
             // this.darkness2.x = this.playertest.x;
             // this.darkness2.y = this.playertest.y;
@@ -76,21 +159,28 @@ class Level1 extends Phaser.Scene {
             // this.darkness3.y = this.playertest.y;
             // this.darkness4.x = this.playertest.x;
             // this.darkness4.y = this.playertest.y;
+        */
+    }
+
+    update() {
+        if(!this.gameOver) {
+            this.p1.update();
         }
-        if(this.playertest.health == 0 && !this.gameOver) {
+        if(this.p1.health == 0 && !this.gameOver) {
             this.gameOver = true;
-            this.playertest.kill();
-            this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'YOU DIED\nPress R to return').setScrollFactor(0);
-        }
-        if(this.EGroups.noneAlive() && !this.gameOver) {
-            this.gameOver = true;
-            this.scene.start('door2Scene')
+            this.p1.kill();
+            this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'YOU DIED\nPress R to retry').setScrollFactor(0);
         }
         if(this.gameOver){
             if (Phaser.Input.Keyboard.JustDown(keyR)) {
-                this.scene.start('titleScene');
+                this.scene.start('door1Scene');
             }
         }
         
+    }
+
+    checkOverlap(p1, tile){
+        if(tile.index == -1) return false;
+        return true;
     }
 }
